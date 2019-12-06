a9f6ecbb888f580973c914408fe800100fcf0496
Nuria Quero <nuriaq@themill.com>
2019-12-06 15:05:06 +0000

    initial commit

---
+++ sketch.js
@@ -0,0 +1,98 @@
+let canvas;
+let img;
+let imgClone;
+let mask;
+
+let numOfLinesOfLines;
+let linelineWidth;
+
+function setup() {
+
+    canvas = createCanvas(800, 800);
+    background(255); //fill(246,234,219);
+    img = createGraphics(800, 800);
+
+    //MASK
+    mask = createGraphics(800, 800);
+    mask.triangle(400, 150, 150, 640, 640, 640);
+
+    //TEXT
+    textSize(18);
+    translate(100, 100);
+    rotate(radians(-90));
+    // text('HAPPY', -310, -40);
+    // text('HOLIDAYS', -330, -20);
+    textFont('Helvetica');
+    text('HAPPY', 20, -70);
+    text('HOLIDAYS', -10, -50);
+
+    translate(100, 100);
+    rotate(radians(180));
+    // text('HAPPY', 350, -570);
+    // text('HOLIDAYS', 335, -550);
+    
+    text('BONES', 712, -570);
+    text('FESTES', 706, -550);
+
+    //INITIAL LINES
+    let numOfLines = random(2, 30);
+    let lineWidth = random(2, 50);
+
+    for (let i = 0; i < numOfLines - 1; i++) {
+
+        let s = random(800);
+        let f = random(-100, 100);
+        img.strokeWeight(lineWidth);
+        img.line(0, s, 800, s + f);
+        img.smooth();
+
+        if (numOfLines <= 10) {
+            lineWidth = 50;
+        }
+    }
+}
+
+function draw() {
+  
+    //where the masking happens
+    (imgClone = img.get()).mask(mask.get());
+    image(imgClone, 0, 0);
+}
+
+function drawLines(){
+    fill(255);
+    //img.noStroke();
+    //img.noFill();
+  
+    let numOfLines = random(2, 30);
+    let lineWidth = random(2, 50);
+
+    for (let i = 0; i < numOfLines; i++) {
+
+        let s = random(800);
+        let f = random(-200, 200);
+       img.strokeWeight(lineWidth);
+         img.line(0, s, 800,  s+f);  // img.line(0, s, 800,  s*f);
+        img.smooth();
+
+        if (numOfLines <= 10) {
+            lineWidth = 50;
+        }
+    }
+}
+
+function mousePressed() {
+   img.rect(-10, -10, 800, 800);
+    noStroke();
+    rect(120, 120, 550, 550);
+    drawLines();
+   
+}
+
+
+function keyPressed() {
+
+    if (keyCode === ENTER) {
+        saveCanvas(canvas, 'myCanvas', 'jpg');
+    }
+}
